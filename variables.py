6d037921f17d6b16a5276552631714b3112b3dfd
kapishan-2005 <kapishan.official@gmail.com>
2025-12-13 17:12:11 +0530

    first commit

---
+++ variables.py
@@ -0,0 +1,19 @@
+# dynamic programming - no need to declare variable type
+# a =10
+# b = "python"
+# c = "10"
+
+# print(a)
+# print(type(a))#int
+# print(b)
+# print(c)
+# print(type(c))#string
+
+# a = True
+# print(a)
+# # print(A)#not defined
+# print(type(a))#boolen
+
+# print (type(5))
+# print (type(0FF))
+print(type([]))
