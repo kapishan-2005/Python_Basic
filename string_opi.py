003fb67ca8ea762342e2bfcea993914db6bb9518
kapishan-2005 <kapishan.official@gmail.com>
2025-12-20 16:59:51 +0530

    upadate

---
+++ string_opi.py
@@ -0,0 +1,23 @@
+#string concatination
+# first_name ="Elon"
+# last_name ="Musk"
+# print(first_name + " " + last_name)
+
+#string replication
+# print("s" *10)
+
+#number of chaarectores in astring
+# str1 ="Hello World"
+# str2 ="Welcome to Python Programming" #space is also a character
+# print(len(str1))
+# print(len(str2))
+
+#indexing
+# str1 ="Hello World"
+# str2 ="Welcome to Python Programming" 
+# print(str1[2])
+# print(str2[11])
+# print(str2[-1])
+# print(str1[5])
+
+
